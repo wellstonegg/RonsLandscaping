08a5b25fd3c4b9800f27f4fd055b17acad68f126
sushilthe <sushilthe@gmail.com>
2018-04-14 16:39:57 +0545

    Send email with sendgrid

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -2,7 +2,7 @@ class ContactUsController < ApplicationController
   def create
     @success = false
     if verify_recaptcha
-      ContactUsMailer.with(contact: contact_us_params).contact_form_email.deliver_later
+      ContactUsMailer.with(contact: contact_us_params.to_json).contact_form_email.deliver_later
       @success = true
     end
     render 'send_message'
--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,6 +1,6 @@
 class ContactUsMailer < ApplicationMailer
   def contact_form_email
-    @contact = params[:contact]
+    @contact = OpenStruct.new(JSON.parse(params[:contact]))
     email_with_name = %("#{@contact.name}" <#{@contact.email}>)
     mail(to: 'sushilthe@gmail.com', subject: @contact.subject, from: email_with_name)
   end
