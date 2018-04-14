8a71ac6c00d19a65ff0852f7fa5bf4e0c9a7f301
sushilthe <sushilthe@gmail.com>
2018-04-14 17:04:16 +0545

    Minor Fixes

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,6 +2,6 @@ class ContactUsMailer < ApplicationMailer
   def contact_form_email
     @contact = OpenStruct.new(JSON.parse(params[:contact]))
     email_with_name = %("#{@contact.name}" <#{@contact.email}>)
-    mail(to: 'sushilthe@gmail.com', subject: @contact.subject, from: email_with_name)
+    mail(to: EMAIL_TO, subject: @contact.subject, from: email_with_name)
   end
 end
--- test/mailers/previews/contact_us_mailer_preview.rb
+++ test/mailers/previews/contact_us_mailer_preview.rb
@@ -1,13 +1,13 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_us_mailer
 class ContactUsMailerPreview < ActionMailer::Preview
   def contact_form_email
-    contact_us_params = OpenStruct.new(name: 'Test User', email: 'test@test.com', phone: '9806543210',
+    contact_us_params = {name: 'Test User', email: 'test@test.com', phone: '9806543210',
                          subject: 'Welcome to Rons Landscaping', message: 'Lorem ipsum dolor sit amet, consectetur
 adipiscing elit. Donec euismod eros nec neque tempor, vitae placerat tortor ullamcorper. Quisque eget nibh vel leo
 aliquet ultrices. Maecenas leo ligula, porttitor sit amet libero vitae, congue pretium erat. Vestibulum ante ipsum
 primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus non maximus mauris, eget auctor augue.
 Donec vestibulum egestas ex, id elementum enim pretium at. Etiam vulputate congue diam, ac molestie ligula semper ac.
-Maecenas convallis lacus nec nibh accumsan commodo. In sagittis lorem nunc.')
+Maecenas convallis lacus nec nibh accumsan commodo. In sagittis lorem nunc.'}.to_json
     ContactUsMailer.with(contact: contact_us_params).contact_form_email
   end
 end
